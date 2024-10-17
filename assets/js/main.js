3c48d235b89bc89c5ac8f85e6bf7cf7338b3b4ef
Ravi Kiran <ravi.kiran8844@gmail.com>
2024-10-17 11:04:13 +0530

    added announcment bar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,23 @@
+ // Array of announcements
+ const announcements = [
+  "CaratLane - A Tanishq Partnership",
+  "New arrivals are now available!",
+  "20% off your first purchase. Use code: WELCOME20",
+];
+
+let index = 0;
+
+function updateAnnouncement() {
+  const announcementElement = document.getElementById("announcement");
+  announcementElement.textContent = announcements[index];
+  index = (index + 1) % announcements.length;
+}
+
+// Initial call
+updateAnnouncement();
+
+// Update every 5 seconds
+setInterval(updateAnnouncement, 5000);
 function toggleWishlist(btn) {
     const icon = btn.querySelector("#wishlistIcon");
     if (icon.getAttribute("fill") === "none") {
@@ -6,3 +26,7 @@ function toggleWishlist(btn) {
       icon.setAttribute("fill", "none");
     }
   }
+
+
+
+ 
